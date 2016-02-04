421f29eb0579507519f7d54b866e1854728baba3
DivineOmega <jordan@hall05.co.uk>
2016-02-04 23:55:38 +0000

    Added basic UPnP port mapping support using weupnp library

--- src/main/network/PeerServer.java
+++ src/main/network/PeerServer.java
@@ -99,17 +99,14 @@ public class PeerServer extends Thread
 		
 		InetAddress localAddress = d.getLocalAddress();
 		String externalIPAddress = d.getExternalIPAddress();
-				
-		PortMappingEntry portMapping = new PortMappingEntry();
 		
 		boolean portMapped = false;
 		
-		// Check if port is already mapped
-		if (d.getSpecificPortMappingEntry(this.port,"TCP",portMapping)) {
-			portMapped = true;	
-		}
-		// Else, attempt to map port
-		else if (d.addPortMapping(this.port, this.port, localAddress.getHostAddress(),"TCP","Decent Messaging")) {
+		// Remove port mapping (if present)
+		d.deletePortMapping(this.port, "TCP");
+		
+		// Attempt to map port
+		if (d.addPortMapping(this.port, this.port, localAddress.getHostAddress(),"TCP","Decent Messaging")) {
 			portMapped = true;
 		}
 		
