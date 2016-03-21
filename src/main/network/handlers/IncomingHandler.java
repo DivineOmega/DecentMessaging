cf7a2bdde1f76b72a0edf4a54acb8caca1dfb5a1
Jordan Hall <jordan.hall@rapidweb.biz>
2016-03-21 14:37:15 +0000

    Node receive protection (cont.)

--- src/main/network/handlers/IncomingHandler.java
+++ src/main/network/handlers/IncomingHandler.java
@@ -93,7 +93,7 @@ public abstract class IncomingHandler
 			String host = in.readUTF();
 			int port = in.readInt();
 			
-			if (host.length() <=4 host.length() >= 80) {
+			if (host.length() <=4 || host.length() >= 80) {
 				return false;
 			}
 			
